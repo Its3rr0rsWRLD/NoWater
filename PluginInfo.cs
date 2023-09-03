83c9b1f2177b58b6ae07fa5b3029fcd07151d8ef
Rascos <132259285+Rascos@users.noreply.github.com>
2023-09-03 10:35:18 +0100

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace NoWater
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.rascosgt.gorillatag.nowater";
+        public const string Name = "NoWater";
+        public const string Version = "1.0.0";
+    }
+}
